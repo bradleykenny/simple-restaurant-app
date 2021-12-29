dccea83e8015bb7130f40fcf6c73560e6d93e156
Bradley Kenny <bkenny283@gmail.com>
2021-12-29 22:19:17 +1100

    Turning into scroll view.

--- src/components/DetailCard/index.tsx
+++ src/components/DetailCard/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
-import { Image, Pressable, SafeAreaView, Text, View } from "react-native";
+import { Image, Pressable, SafeAreaView, ScrollView,Text, View } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 import { SharedElement } from "react-navigation-shared-element";
 import { HomeTabStackType } from "../../navigation/HomeTab";
@@ -38,30 +38,32 @@ const DetailCard = (props: IDetailCardProps) => {
 					</View>
 				</SharedElement>
 			)}
-			<SafeAreaView>
-				<Pressable
-					style={styles.pressable}
-					onPress={handlePress}
-				>
-					<Icon name="close" size={16} color="#fff">
-						<Text style={styles.iconText}> Close </Text>
-					</Icon>
-				</Pressable>
-				<View style={styles.paddedInnerCard}>
-					<Text style={styles.title}>
-						{business.name}
-					</Text>
-					<Text>
-						Rating: {business.rating}/5
-					</Text>
-					<Text>
-						Price: {business.price}
-					</Text>
-					<TextField label='First Name' placeholder='John' />
-					<TextField label='Last Name' placeholder='Apple' />
-				</View>
-				<StatusBar style="light" />
-			</SafeAreaView>
+			<ScrollView>
+				<SafeAreaView>
+					<Pressable
+						style={styles.pressable}
+						onPress={handlePress}
+					>
+						<Icon name="close" size={16} color="#fff">
+							<Text style={styles.iconText}> Close </Text>
+						</Icon>
+					</Pressable>
+					<View style={styles.paddedInnerCard}>
+						<Text style={styles.title}>
+							{business.name}
+						</Text>
+						<Text>
+							Rating: {business.rating}/5
+						</Text>
+						<Text>
+							Price: {business.price}
+						</Text>
+						<TextField label='First Name' placeholder='John' />
+						<TextField label='Last Name' placeholder='Apple' />
+					</View>
+					<StatusBar style="light" />
+				</SafeAreaView>
+			</ScrollView>
 		</View>
 	);
 };
